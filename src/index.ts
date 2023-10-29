31f0367fd43582d1afdb01cf6797ab680d3fa787
yymin1022 <yymin1022@gmail.com>
2023-10-30 04:24:26 +0900

    Update index.ts : Applied JSON/URLEncoded to Express

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import {setCors} from "./utils/cors";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
 setCors(app);
 
 app.get("/", (req, res) => {
