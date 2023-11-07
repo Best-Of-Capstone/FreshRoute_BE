e9092138d1e469faf9aa8ff072d41c5f4f9794ed
falconlee236 <falconlee236@cau.ac.kr>
2023-11-07 13:31:41 +0900

    modify FindRouteAPI.ts: handle Error Catch

--- src/Routes/FindRouteAPI.ts
+++ src/Routes/FindRouteAPI.ts
@@ -11,31 +11,40 @@ findRouteRouter.post("/", async (req: Request, res: Response) => {
         RESULT_MSG: "Ready",
         RESULT_DATA: {}
     }
+
     if (req.body?.startCord === undefined || req.body?.endCord === undefined) {
         RESULT_DATA['RESULT_CODE'] = 400;
         RESULT_DATA['RESULT_MSG'] = "body must have startCord property and endCord property";
         res.send(RESULT_DATA);
     }
+
     coordinatesList.push(req.body.startCord);
     if (req.body?.stopover !== undefined) {
         coordinatesList.push(...req.body.stopover);
     }
     coordinatesList.push(req.body.endCord);
-    const routeMsg = await axios.post(URL, {
-        coordinates: coordinatesList
-    }, {
-        headers: {
-            Authorization: process.env.OPENROUTESERVICE_KEY
-        },
-    })
-    RESULT_DATA["RESULT_CODE"] = routeMsg.status
-    RESULT_DATA["RESULT_MSG"] = routeMsg.statusText
-    if (routeMsg.status === 200) {
-        RESULT_DATA['RESULT_DATA'] = {
-            coordinates: routeMsg.data.features[0].geometry.coordinates,
+
+    try {
+        const routeMsg = await axios.post(URL, {
+            coordinates: coordinatesList
+        }, {
+            headers: {
+                Authorization: process.env.OPENROUTESERVICE_KEY
+            },
+        });
+        RESULT_DATA["RESULT_CODE"] = routeMsg.status;
+        RESULT_DATA["RESULT_MSG"] = routeMsg.statusText;
+        if (routeMsg.status === 200) {
+            RESULT_DATA['RESULT_DATA'] = {
+                coordinates: routeMsg.data.features[0].geometry.coordinates,
+            }
         }
+        res.send(RESULT_DATA);
+    } catch (err) {
+        RESULT_DATA["RESULT_CODE"] = 400;
+        RESULT_DATA["RESULT_MSG"] = err instanceof Error ? err.message : "400 not found";
+        res.send(RESULT_DATA);
     }
-    res.send(RESULT_DATA);
 });
 
 export default findRouteRouter;
