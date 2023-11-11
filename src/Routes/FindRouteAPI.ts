e5ac3b04412b1024cde0f8242aaf7066b7dd0d2b
falconlee236 <falconlee236@cau.ac.kr>
2023-11-11 14:17:01 +0900

    modify FindRouteAPI.ts: add instructionType object

--- src/Routes/FindRouteAPI.ts
+++ src/Routes/FindRouteAPI.ts
@@ -16,6 +16,23 @@ findRouteRouter.post("/", async (req: Request, res: Response) => {
         "weight_factor": 2,
         "share_factor": 1
     };
+    const instructionTypes = {
+        0: "좌회전", //Left
+        1: "우회전", //Right
+        2: "7시 방향", //Sharp left
+        3: "5시 방향", //Sharp right
+        4: "11시 방향", //Slight left
+        5: "1시 방향", //Slight right
+        6: "직진", //Straight
+        7: "회전교차로 진입", //Enter roundabout
+        8: "회전교차로 탈출", //Exit roundabout
+        9: "유턴", //U-turn
+        10: "목적지 도착", //Goal
+        11: "출발", //Depart
+        12: "왼쪽으로 크게 돌기", //keep left
+        13: "오른쪽으로 크게 돌기", //keep right
+
+    }
 
     alternativeRoutesConfig["target_count"] = req.body?.targetCount ?? alternativeRoutesConfig["target_count"];
     alternativeRoutesConfig["weight_factor"] = req.body?.targetCount ?? alternativeRoutesConfig["weight_factor"];
@@ -47,16 +64,20 @@ findRouteRouter.post("/", async (req: Request, res: Response) => {
         RESULT_DATA["RESULT_MSG"] = routeMsg.statusText;
         if (routeMsg.status === 200) {
             RESULT_DATA['RESULT_DATA'] = {
-                routeList: routeMsg.data.features.map(({geometry: geometry}: any, index: number) => {
+                routeList: routeMsg.data.features.map(({
+                                                           geometry: geometry,
+                                                           properties: properties
+                                                       }: any, index: number) => {
+                    const detailRouteInfo = properties.segments[0];
                     return {
                         id: index,
                         description: `Route ${index}`,
                         route: {
+                            distance: detailRouteInfo.distance,
+                            duration: detailRouteInfo.duration,
+                            steps: detailRouteInfo.steps,
                             coordinates: geometry.coordinates.map((coordinate: [number, number]) => {
-                                return {
-                                    description: "walking",
-                                    coordinate: [coordinate[1], coordinate[0]]
-                                };
+                                return [coordinate[1], coordinate[0]];
                             }),
                         }
                     }
