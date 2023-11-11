743c0e2ef8a00124e654a10f06e4998b4d79e020
falconlee236 <falconlee236@cau.ac.kr>
2023-11-11 13:16:54 +0900

    modify FindRouteAPI.ts: Add Checking AxiosError handling Code

--- src/Routes/FindRouteAPI.ts
+++ src/Routes/FindRouteAPI.ts
@@ -1,21 +1,25 @@
 import express, {Request, Response} from "express";
-import axios from "axios";
+import axios, {AxiosError} from "axios";
 
 const findRouteRouter = express.Router();
 
 findRouteRouter.post("/", async (req: Request, res: Response) => {
     const URL: string = "https://api.openrouteservice.org/v2/directions/foot-walking/geojson";
-    const coordinatesList = [];
+    const coordinatesList: [number, number][] = [];
     const RESULT_DATA = {
         RESULT_CODE: 0,
         RESULT_MSG: "Ready",
         RESULT_DATA: {}
     }
-    const alternativeRoutesObject = {
+    const alternativeRoutesConfig = {
         "target_count": 3,
         "weight_factor": 2,
         "share_factor": 1
     };
+
+    alternativeRoutesConfig["target_count"] = req.body?.targetCount ?? alternativeRoutesConfig["target_count"];
+    alternativeRoutesConfig["weight_factor"] = req.body?.targetCount ?? alternativeRoutesConfig["weight_factor"];
+    alternativeRoutesConfig["share_factor"] = req.body?.targetCount ?? alternativeRoutesConfig["share_factor"];
 
     if (req.body?.startCord === undefined || req.body?.endCord === undefined) {
         RESULT_DATA['RESULT_CODE'] = 400;
@@ -31,20 +35,19 @@ findRouteRouter.post("/", async (req: Request, res: Response) => {
     }
     coordinatesList.push([req.body.endCord[1], req.body.endCord[0]]);
 
-    alternativeRoutesObject["target_count"] = req.body?.targetCount ?? alternativeRoutesObject["target_count"];
-    alternativeRoutesObject["weight_factor"] = req.body?.targetCount ?? alternativeRoutesObject["weight_factor"];
-    alternativeRoutesObject["share_factor"] = req.body?.targetCount ?? alternativeRoutesObject["share_factor"];
     //weight_factor 1~2, share_factor 0.1~1
     try {
-        const routeMsg = await axios.post(URL, {
+        const body = alternativeRoutesConfig.target_count === 1 ? {
             "coordinates": coordinatesList,
-            "alternative_routes": alternativeRoutesObject,
-        }, {
+        } : {
+            "coordinates": coordinatesList,
+            "alternative_routes": alternativeRoutesConfig,
+        }
+        const routeMsg = await axios.post(URL, body, {
             headers: {
                 Authorization: process.env.OPENROUTESERVICE_KEY
             },
         });
-        console.log(routeMsg.status);
         RESULT_DATA["RESULT_CODE"] = routeMsg.status;
         RESULT_DATA["RESULT_MSG"] = routeMsg.statusText;
         if (routeMsg.status === 200) {
@@ -59,9 +62,11 @@ findRouteRouter.post("/", async (req: Request, res: Response) => {
         }
         res.send(RESULT_DATA);
     } catch (err) {
-        RESULT_DATA["RESULT_CODE"] = 400;
-        RESULT_DATA["RESULT_MSG"] = err instanceof Error ? err.message : "400 not found";
-        res.send(RESULT_DATA);
+        if (err instanceof AxiosError) {
+            RESULT_DATA["RESULT_CODE"] = err.response?.status ?? 404;
+            RESULT_DATA["RESULT_MSG"] = err.response?.data.error.message ?? "Internal Server Error";
+            res.send(RESULT_DATA);
+        }
     }
 });
 
