bb9195aa1b299e77cd2070e9c496717e2cca4086
yymin1022 <yymin1022@gmail.com>
2023-11-13 21:53:53 +0900

    Update Weather API : Fixed Parameter Type

--- src/Routes/WeatherAPI.ts
+++ src/Routes/WeatherAPI.ts
@@ -4,8 +4,8 @@ const weatherRouter = express.Router();
 
 weatherRouter.get("/getWeatherInfo", async (req: Request, res: Response) => {
     const API_KEY = process.env.OPENWEATHER_KEY;
-    const lat = req.params.lat;
-    const lon = req.params.lon;
+    const lat = req.query.lat;
+    const lon = req.query.lon;
 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
