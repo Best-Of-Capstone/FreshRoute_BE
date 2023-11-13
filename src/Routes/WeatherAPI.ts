425dfb74e8d5ec1a3ae2cca11ff3a8962b658210
yymin1022 <yymin1022@gmail.com>
2023-11-13 21:41:08 +0900

    Update Weather API : Added Base Variables for Weather API

--- src/Routes/WeatherAPI.ts
+++ src/Routes/WeatherAPI.ts
@@ -3,6 +3,12 @@ import express, { Request, Response } from "express";
 const weatherRouter = express.Router();
 
 weatherRouter.get("/getWeatherInfo", async (req: Request, res: Response) => {
+    const API_KEY = process.env.OPENWEATHER_KEY;
+    const lat = req.params.lat;
+    const lon = req.params.lon;
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
     res.send("Get Weather API Router");
 });
 
