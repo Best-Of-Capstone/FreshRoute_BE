400eeca0564472c3682ccfe6f0019798b2015277
yymin1022 <yymin1022@gmail.com>
2023-11-30 20:58:22 +0900

    Update FindRoute API : Cleaned UP

--- src/Routes/FindRouteAPI.ts
+++ src/Routes/FindRouteAPI.ts
@@ -4,95 +4,6 @@ import {ResultMSGDTO, RouteBodyDTO, RouteListDTO, RouteStepDTO, StepDTO} from ".
 
 const findRouteRouter = express.Router();
 
-const findWalkRoute = async (body: any): Promise<ResultMSGDTO> => {
-    const instructionTypes = [
-        "좌회전", //Left
-        "우회전", //Right
-        "7시 방향", //Sharp left
-        "5시 방향", //Sharp right
-        "11시 방향", //Slight left
-        "1시 방향", //Slight right
-        "직진", //Straight
-        "회전교차로 진입", //Enter roundabout
-        "회전교차로 탈출", //Exit roundabout
-        "유턴", //U-turn
-        "목적지 도착", //Goal
-        "출발", //Depart
-        "왼쪽으로 크게 돌기", //keep left
-        "오른쪽으로 크게 돌기" //keep right
-    ];
-    const routeURL: string = "https://api.openrouteservice.org/v2/directions/foot-walking/geojson";
-    const RESULT_DATA: ResultMSGDTO = {
-        RESULT_CODE: 0,
-        RESULT_MSG: "Ready",
-        RESULT_DATA: {}
-    }
-    const routeMsg = await axios.post(routeURL, body, {
-        headers: {
-            Authorization: process.env.OPENROUTESERVICE_KEY
-        },
-    });
-    RESULT_DATA["RESULT_CODE"] = routeMsg.status;
-    RESULT_DATA["RESULT_MSG"] = routeMsg.statusText;
-    try {
-        if (routeMsg.status === 200) {
-            const routeList = await Promise.all(routeMsg.data.features.map(async ({
-                                                                                      geometry: geometry,
-                                                                                      properties: properties
-                                                                                  }: any, index: number) => {
-                const detailRouteInfo = properties.segments[0];
-                const elevationList = geometry.coordinates.map((coordinate: [number, number, number]) => {
-                    return coordinate[2];
-                });
-
-                const detailStepInfo = await Promise.all(detailRouteInfo.steps.map(async (step: RouteStepDTO) => {
-                    const [longitude, latitude] = geometry.coordinates[step.way_points[0]];
-                    const REVERSE_GEOCODING_URL: string = `https://api.vworld.kr/req/address?request=getAddress&` +
-                        `service=address&point=${longitude},${latitude}&` +
-                        `type=parcel&zipcode=false&key=${process.env.GEOCODING_KEY}`;
-                    const reverseGeocodeResultResponse = await axios.get(REVERSE_GEOCODING_URL);
-                    const textData = reverseGeocodeResultResponse.data.response?.result[0].text ?? "-";
-                    return {
-                        distance: step.distance,
-                        duration: step.duration,
-                        type: instructionTypes[step.type],
-                        name: textData,
-                        elevationDelta: elevationList[step.way_points[1]] - elevationList[step.way_points[0]],
-                        wayPoints: step.way_points,
-                        isWalking: true,
-                    }
-                }));
-
-                return {
-                    id: index,
-                    description: `Route ${index}`,
-                    route: {
-                        distance: detailRouteInfo.distance,
-                        duration: detailRouteInfo.duration,
-                        ascent: properties.ascent,
-                        descent: properties.descent,
-                        steps: detailStepInfo,
-                        coordinates: geometry.coordinates.map((coordinate: [number, number, number]) => {
-                            return [coordinate[1], coordinate[0], coordinate[2]];
-                        }),
-                    }
-                }
-            }));
-            RESULT_DATA['RESULT_DATA'] = {
-                routeList: routeList
-            }
-        }
-        return RESULT_DATA;
-    } catch (err) {
-        if (err instanceof AxiosError) {
-            RESULT_DATA["RESULT_CODE"] = err.response?.status ?? 404;
-            RESULT_DATA["RESULT_MSG"] = err.response?.data.error.message ?? "Internal Server Error";
-            return RESULT_DATA;
-        }
-        return RESULT_DATA;
-    }
-}
-
 findRouteRouter.post("/", async (req: Request, res: Response) => {
     const transportURL: string = "https://asia-northeast3-spring-market-404709.cloudfunctions.net/function-2"
     const coordinatesList: [number, number][] = [];
@@ -230,5 +141,94 @@ findRouteRouter.post("/", async (req: Request, res: Response) => {
         }
     }
 });
+
+const findWalkRoute = async (body: any): Promise<ResultMSGDTO> => {
+    const instructionTypes = [
+        "좌회전", //Left
+        "우회전", //Right
+        "7시 방향", //Sharp left
+        "5시 방향", //Sharp right
+        "11시 방향", //Slight left
+        "1시 방향", //Slight right
+        "직진", //Straight
+        "회전교차로 진입", //Enter roundabout
+        "회전교차로 탈출", //Exit roundabout
+        "유턴", //U-turn
+        "목적지 도착", //Goal
+        "출발", //Depart
+        "왼쪽으로 크게 돌기", //keep left
+        "오른쪽으로 크게 돌기" //keep right
+    ];
+    const routeURL: string = "https://api.openrouteservice.org/v2/directions/foot-walking/geojson";
+    const RESULT_DATA: ResultMSGDTO = {
+        RESULT_CODE: 0,
+        RESULT_MSG: "Ready",
+        RESULT_DATA: {}
+    }
+    const routeMsg = await axios.post(routeURL, body, {
+        headers: {
+            Authorization: process.env.OPENROUTESERVICE_KEY
+        },
+    });
+    RESULT_DATA["RESULT_CODE"] = routeMsg.status;
+    RESULT_DATA["RESULT_MSG"] = routeMsg.statusText;
+    try {
+        if (routeMsg.status === 200) {
+            const routeList = await Promise.all(routeMsg.data.features.map(async ({
+                                                                                      geometry: geometry,
+                                                                                      properties: properties
+                                                                                  }: any, index: number) => {
+                const detailRouteInfo = properties.segments[0];
+                const elevationList = geometry.coordinates.map((coordinate: [number, number, number]) => {
+                    return coordinate[2];
+                });
+
+                const detailStepInfo = await Promise.all(detailRouteInfo.steps.map(async (step: RouteStepDTO) => {
+                    const [longitude, latitude] = geometry.coordinates[step.way_points[0]];
+                    const REVERSE_GEOCODING_URL: string = `https://api.vworld.kr/req/address?request=getAddress&` +
+                        `service=address&point=${longitude},${latitude}&` +
+                        `type=parcel&zipcode=false&key=${process.env.GEOCODING_KEY}`;
+                    const reverseGeocodeResultResponse = await axios.get(REVERSE_GEOCODING_URL);
+                    const textData = reverseGeocodeResultResponse.data.response?.result[0].text ?? "-";
+                    return {
+                        distance: step.distance,
+                        duration: step.duration,
+                        type: instructionTypes[step.type],
+                        name: textData,
+                        elevationDelta: elevationList[step.way_points[1]] - elevationList[step.way_points[0]],
+                        wayPoints: step.way_points,
+                        isWalking: true,
+                    }
+                }));
+
+                return {
+                    id: index,
+                    description: `Route ${index}`,
+                    route: {
+                        distance: detailRouteInfo.distance,
+                        duration: detailRouteInfo.duration,
+                        ascent: properties.ascent,
+                        descent: properties.descent,
+                        steps: detailStepInfo,
+                        coordinates: geometry.coordinates.map((coordinate: [number, number, number]) => {
+                            return [coordinate[1], coordinate[0], coordinate[2]];
+                        }),
+                    }
+                }
+            }));
+            RESULT_DATA['RESULT_DATA'] = {
+                routeList: routeList
+            }
+        }
+        return RESULT_DATA;
+    } catch (err) {
+        if (err instanceof AxiosError) {
+            RESULT_DATA["RESULT_CODE"] = err.response?.status ?? 404;
+            RESULT_DATA["RESULT_MSG"] = err.response?.data.error.message ?? "Internal Server Error";
+            return RESULT_DATA;
+        }
+        return RESULT_DATA;
+    }
+}
 
 export default findRouteRouter;
